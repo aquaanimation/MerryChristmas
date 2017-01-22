be443d418e47f45acdb2bec3d3a62c176fad3cd7
Alex G.G <aquaanimation@gmail.com>
2017-01-22 17:45:27 +0200

    Toggle sound with button

--- app/src/main/java/com/example/android/happybirthday/MainActivity.java
+++ app/src/main/java/com/example/android/happybirthday/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.res.Resources;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 
 public class MainActivity extends AppCompatActivity {
@@ -13,8 +16,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Resources res = getResources();
         setContentView(R.layout.activity_main);
-        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.merry_xmas_song);
-        mediaPlayer.start();
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.merry_xmas_song);
+
+        final Button toggleSoundBtn = (Button) findViewById(R.id.toggleSound);
+
+        toggleSoundBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (!mediaPlayer.isPlaying()) {
+                    mediaPlayer.start();
+                }else{
+                    mediaPlayer.reset();
+                    mediaPlayer.pause();
+                }
+
+            }
+        });
+
+
 
         }
 
